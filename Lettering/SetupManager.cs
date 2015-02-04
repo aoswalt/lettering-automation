30800de953b3e1a6ee31fa1285b722edf70d9753
Adam Oswalt <adam.oswalt@gmail.com>
2015-02-04 15:22:30 -0600

    Changed to temporary font loading with prompting.

--- Lettering/SetupManager.cs
+++ Lettering/SetupManager.cs
@@ -15,9 +15,10 @@ namespace Lettering {
     class SetupManager {
         private static string installedLibPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + 
                 @"\Corel\CorelDRAW Graphics Suite X7\Draw\GMS\Automation.gms";
-        private static string libPath = @"\\production\lettering\Corel\WORK FOLDERS\Automation\Automation.gms";
+        private static string libPath = @"\\production\Lettering\Corel\WORK FOLDERS\Automation\Automation.gms";
         private static string installedFontFolder = Environment.GetFolderPath(Environment.SpecialFolder.Fonts);
-        private static string fontFolder = @"\\production\lettering\Corel\WORK FOLDERS\VS Fonts";
+        private static string fontFolder = @"\\production\Lettering\Corel\WORK FOLDERS\VS Fonts";
+        private static string missingFonts = "";
 
         // font installer function
         [DllImport("gdi32.dll", EntryPoint = "AddFontResourceW", SetLastError = true)]
@@ -29,13 +30,25 @@ namespace Lettering {
             bool fontInstall = InstallFonts();
 
             string msg = "";
-            msg += (libInstall ? "Library had to be updated.\n" : "");
-            //msg += (fontInstall ? "Font(s) had to be updated.\n" : "");
+            msg += (libInstall ? "Library had to be updated.\n\n" : "");
+            msg += (fontInstall ? "Font(s) missing or need to be updated:\n" + missingFonts : "");
             msg += "\nCorel must be restarted before continuing.";
             msg += "\n\nPlease save all work and press OK to close Corel.";
 
-            if(libInstall && Lettering.corel.Visible) {
-                MessageBox.Show(msg, "Corel Restart Required", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if(libInstall || fontInstall) {
+                if(Lettering.corel.Visible) {
+                    MessageBox.Show(msg, "Corel Restart Required", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // set all documents as clean to prevent error on quit
+                    foreach(VGCore.Document document in Lettering.corel.Documents) {
+                        document.Dirty = false;
+                    }
+
+                    Lettering.corel.Quit();
+                } else {
+                    MessageBox.Show("Font(s) need to be installed or updated:\n" + missingFonts, "Missing Fonts", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
                 return false;
             } else {
                 return true;
@@ -72,56 +85,40 @@ namespace Lettering {
                 List<string> installedFonts = GetInstalledFonts();
 
                 string fontName = GetFontNameFromFile(fontFileName);
-                string installedFontFile = "";
                 if(!installedFonts.Contains(fontName)) {
                     // font not installed
+                    MessageBox.Show(fontName + " not installed.");
+                    missingFonts += fontName + "\n";
                     needFontInstall = true;
                 } else {
                     // font installed, check file date
-                    installedFontFile = GetInstalledFontFileName(fontName);
+                    string installedFontFile = GetInstalledFontFileName(fontName);
 
                     if(installedFontFile == null) {
+                        // can't find font file (shouldn't happen)
+                        MessageBox.Show(fontName + " installed file name not found.");
+                        missingFonts += fontName + "\n";
                         needFontInstall = true;
                     } else {
-                        DateTime installedFontDT = File.GetLastWriteTime(installedFontFile);
+                        DateTime installedFontDT = File.GetLastWriteTime(installedFontFolder + "\\" + installedFontFile);
                         DateTime fontDT = File.GetLastWriteTime(fullFontPath);
 
                         // is saved is newer than installed
                         if(fontDT > installedFontDT) {
+                            // font out of date
+                            MessageBox.Show(fontName + " has date difference.\nServer:    " + fontDT + "\nInstalled: " + installedFontDT);
+                            missingFonts += fontName + "\n";
                             needFontInstall = true;
                         }
                     }
                 }
-                string newInstalledFontFileName = installedFontFile.Length > 0 ? installedFontFile.Insert(installedFontFile.Length - 4, "_") :
-                                                                                 fontFileName.Insert(fontFileName.Length - 4, "_");
-                string newInstalledFontPath = installedFontFolder + '\\' + newInstalledFontFileName;
 
                 if(needFontInstall) {
-                    // cannot install fonts if corel is open
-                    if(Lettering.corel.Visible) {
-                        MessageBox.Show("Fonts must be updated. Please save all documents and press OK to close Corel.", "Font Update Required");
-
-                        // set all documents as clean to prevent error on quit
-                        foreach(VGCore.Document document in Lettering.corel.Documents) {
-                            document.Dirty = false;
-                        }
-
-                        Lettering.corel.Quit();
-                    }
-
-                    // copy to font folder, then install font to current session
-                    File.Copy(fullFontPath, newInstalledFontPath, true);
-
-                    int result = AddFontResource(newInstalledFontPath);
+                    int result = AddFontResource(fullFontPath);
                     int error = Marshal.GetLastWin32Error();
 
-                    if(error != 0) {
-                        MessageBox.Show(error + "\n" + new Win32Exception(error).Message + "\n\n" + fullFontPath);
-                    } else {
-                        // add to registry
-                        RegistryKey key = Registry.LocalMachine.CreateSubKey(@"Software\Microsoft\Windows NT\CurrentVersion\Fonts");
-                        key.SetValue(fontName + " (TrueType)", Path.GetFileName(newInstalledFontPath));
-                        key.Close();
+                    if(error != 0 && error != 1400) {
+                        MessageBox.Show("Error no: " + error + "\n" + new Win32Exception(error).Message + "\n\n" + fullFontPath);
                     }
                 }
             }
