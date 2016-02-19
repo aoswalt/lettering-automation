f6e12c0c40b62614cec7ff4180682c9226c6e884
Adam Oswalt <adam.oswalt@gmail.com>
2016-02-19 09:22:58 -0600

    Added FieldData for manipulating and maintaing individual report fields. Adapted CsvReader to allow reading of varying layouts.

---
+++ Lettering/Data/FieldData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+
+namespace Lettering.Data {
+    class FieldData {
+        public static ReadOnlyCollection<FieldData> List { get { return privateList.AsReadOnly(); } }
+        private static List<FieldData> privateList = new List<FieldData>();
+
+        public static readonly FieldData CUT_HOUSE =     new FieldData("DHOUS",    "Cut House",     typeof(string),   new string[] { "house", "hse" });
+        public static readonly FieldData SCHEDULE_DATE = new FieldData("SCDAT",    "Schedule Date", typeof(DateTime), new string[] { "schedule", "sch*MMDDCCYY", "sch" });
+        public static readonly FieldData ENTER_DATE =    new FieldData("ENDAT",    "Enter Date",    typeof(DateTime), new string[] { "enter", "order*date", "order*dt" });
+        public static readonly FieldData ORDER_NUMBER =  new FieldData("ORDNR",    "Order Number",  typeof(int),      new string[] { "order" });
+        public static readonly FieldData VOUCHER =       new FieldData("ORVCH",    "Voucher",       typeof(int),      new string[] { "voucher", "vch" });
+        public static readonly FieldData ITEM =          new FieldData("DITEM",    "Item",          typeof(string),   new string[] { "item", "style", "code" });
+        public static readonly FieldData SIZE =          new FieldData("DLSIZ",    "Size",          typeof(double),   new string[] { "size", "sze" });
+        public static readonly FieldData SPEC =          new FieldData("LETWID",   "Spec",          typeof(double),   new string[] { "spec", "width" });
+        public static readonly FieldData NAME =          new FieldData("LETNAME",  "Name",          typeof(string),   new string[] { "name" });
+        public static readonly FieldData WORD2 =         new FieldData("DLWR1",    "Word 1",        typeof(string),   new string[] { "word*1" });
+        public static readonly FieldData WORD3 =         new FieldData("DLWR2",    "Word 2",        typeof(string),   new string[] { "word*2" });
+        public static readonly FieldData WORD4 =         new FieldData("DLWR3",    "Word 3",        typeof(string),   new string[] { "word*3" });
+        public static readonly FieldData WORD1 =         new FieldData("DLWR4",    "Word 4",        typeof(string),   new string[] { "word*4" });
+        public static readonly FieldData COLOR1 =        new FieldData("DCLR1",    "Color 1",       typeof(string),   new string[] { "color*1" });
+        public static readonly FieldData COLOR2 =        new FieldData("DCLR2",    "Color 2",       typeof(string),   new string[] { "color*2" });
+        public static readonly FieldData COLOR3 =        new FieldData("DCLR3",    "Color 3",       typeof(string),   new string[] { "color*3" });
+        public static readonly FieldData COLOR4 =        new FieldData("DCLR4",    "Color 4",       typeof(string),   new string[] { "color*4" });
+        public static readonly FieldData RUSH_DATE =     new FieldData("RUDAT",    "Rush Date",     typeof(DateTime), new string[] { "rush", "rsh" });
+        public static readonly FieldData COMMENTS =      new FieldData("COMMENTS", "Comment",       typeof(DateTime), new string[] { "comment" });
+
+        internal readonly string DbName;
+        internal readonly string DisplayName;
+        internal readonly Type FieldType;
+
+        private string[] tryMatches;
+
+        internal FieldData(string DbName, string DisplayName, Type FieldType, string[] rawMatches) {
+            this.DbName = DbName.ToUpper();
+            this.DisplayName = DisplayName;
+            this.FieldType = FieldType;
+
+            tryMatches = new string[rawMatches.Length];
+
+            for(int i = 0; i != rawMatches.Length; ++i) {
+                //NOTE(adam): convert simple wildcards to regex wildcards and add surrounding matches
+                string pattern = rawMatches[i].ToUpper().Replace("?", ".").Replace("*", "(.+)?");
+                pattern = $"(.+)?{pattern}(.+)?";
+                tryMatches[i] = pattern;
+            }
+
+            FieldData.privateList.Add(this);
+        }
+
+        internal bool IsMatch(string input) {
+            foreach(string lookupString in tryMatches) {
+                if(input.ToUpper() == DbName || Regex.IsMatch(input.ToUpper(), lookupString)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- Lettering/IO/CsvReader.cs
+++ Lettering/IO/CsvReader.cs
@@ -2,10 +2,7 @@
 using System.Data;
 using System.Data.Odbc;
 using System.IO;
-using System.Text.RegularExpressions;
-using System.Windows.Forms;
 using Lettering.Data;
-using Lettering.Errors;
 
 namespace Lettering.IO {
     internal class CsvReader {
@@ -13,156 +10,50 @@ namespace Lettering.IO {
             //NOTE(adam): operating on copy to prevent locking network file
             string fileName = Path.GetFileName(chosenCsvFilePath);
             Directory.CreateDirectory(FilePaths.tempFolderPath);
-            File.Copy(chosenCsvFilePath, FilePaths.tempFolderPath + fileName, true);
+            string tempCsvFilePath = FilePaths.tempFolderPath + fileName;
+            File.Copy(chosenCsvFilePath, tempCsvFilePath, true);
 
-            string query = @"SELECT * FROM [" + fileName + "]";
+            //NOTE(adam): modifies first line of csv to have proper headers
+            string[] lines = File.ReadAllLines(tempCsvFilePath);
+            lines[0] = RenameHeaders(lines[0]);
+            File.WriteAllLines(tempCsvFilePath, lines);
 
             using(OdbcConnection conn = new OdbcConnection("Driver={Microsoft Text Driver (*.txt; *.csv)};DBQ=" + FilePaths.tempFolderPath)) {
+                string query = @"SELECT * FROM [" + fileName + "]";
                 OdbcCommand command = new OdbcCommand(query, conn);
                 OdbcDataAdapter adapter = new OdbcDataAdapter(command);
 
+                //NOTE(adam): pre-setting fields to enforce types
                 DataTable dataTable = new DataTable();
-                DataColumn[] cols = {
-                        new DataColumn("HOUSE", typeof(string)),
-                        new DataColumn("ORDER_NO", typeof(int)),
-                        new DataColumn("ORDER_VOUCH", typeof(int)),
-                        new DataColumn("PARENT_VOUCH", typeof(int)),
-                        new DataColumn("ITEM_NO", typeof(string)),
-                        new DataColumn("LETTER_SIZE", typeof(double)),
-                        new DataColumn("LETTER_SPEC", typeof(double)),
-                        new DataColumn("DRAWING_LETTER_WORD1", typeof(string)),
-                        new DataColumn("DRAWING_LETTER_WORD2", typeof(string)),
-                        new DataColumn("DRAWING_LETTER_WORD3", typeof(string)),
-                        new DataColumn("DRAWING_LETTER_WORD4", typeof(string)),
-                        new DataColumn("SCHEDULE_DATE_CCYYMMDD", typeof(int)),
-                        new DataColumn("SCHEDULE_DATE_MMDDCCYY", typeof(DateTime)),
-                        new DataColumn("NAME", typeof(string))
-                    };
+                DataColumn[] cols = new DataColumn[FieldData.List.Count];
+                for(int i = 0; i != cols.Length; ++i){
+                    FieldData field = FieldData.List[i];
+                    cols[i] = new DataColumn(field.DbName, field.FieldType);
+                }
                 dataTable.Columns.AddRange(cols);
 
-                try {
-                    adapter.Fill(dataTable);
-                } catch(InvalidCastException) {
+                adapter.Fill(dataTable);
+                File.Delete(tempCsvFilePath);
 
-                    DataTable dtClone = dataTable.Clone();
-                    dtClone.Columns["HOUSE"].DataType = typeof(string);
-                    dtClone.Columns["SCHEDULE_DATE_MMDDCCYY"].DataType = typeof(DateTime);
-                    dtClone.Columns["ORDER_NO"].DataType = typeof(int);
-                    dtClone.Columns["ORDER_VOUCH"].DataType = typeof(int);
-                    dtClone.Columns["ITEM_NO"].DataType = typeof(string);
-                    dtClone.Columns["LETTER_SIZE"].DataType = typeof(double);
-                    dtClone.Columns["LETTER_SPEC"].DataType = typeof(double);
-                    dtClone.Columns["DRAWING_LETTER_WORD1"].DataType = typeof(string);
-                    dtClone.Columns["DRAWING_LETTER_WORD2"].DataType = typeof(string);
-                    dtClone.Columns["DRAWING_LETTER_WORD3"].DataType = typeof(string);
-                    dtClone.Columns["DRAWING_LETTER_WORD4"].DataType = typeof(string);
-                    dtClone.Columns["NAME"].DataType = typeof(string);
-
-                    foreach(DataRow row in dataTable.Rows) {
-                        object[] vals = row.ItemArray;
-
-                        vals[0] = ((string)vals[0]).Replace("\"", "");                      // HOUSE
-                        vals[1] = int.Parse(((string)vals[1]).Replace("\"", ""));           // ORDER_NO
-                        vals[2] = int.Parse(((string)vals[2]).Replace("\"", ""));           // ORDER_VOUCH
-                        vals[3] = int.Parse(((string)vals[3]).Replace("\"", ""));           // PARENT_VOUCH
-                        vals[4] = ((string)vals[4]).Replace("\"", "");                      // ITEM_NO
-                        vals[5] = double.Parse(((string)vals[5]).Replace("\"", ""));        // LETTER_SIZE
-                        vals[6] = double.Parse(((string)vals[6]).Replace("\"", ""));        // LETTER_SPEC
-                        vals[7] = ((string)vals[7]).Replace("\"", "");                      // DRAWING_LETTER_WORD1
-                        vals[8] = ((string)vals[8]).Replace("\"", "");                      // DRAWING_LETTER_WORD2
-                        vals[9] = ((string)vals[9]).Replace("\"", "");                      // DRAWING_LETTER_WORD3
-                        vals[10] = ((string)vals[10]).Replace("\"", "");                    // DRAWING_LETTER_WORD4
-                        vals[11] = int.Parse(((string)vals[11]).Replace("\"", ""));         // SCHEDULE_DATE_CCYYMMDD
-                        vals[12] = DateTime.Parse(((string)vals[12]).Replace("\"", ""));    // SCHEDULE_DATE_MMDDCCYY
-                        vals[13] = ((string)vals[13]).Replace("\"", "");                    // NAME
-
-                        dtClone.NewRow().ItemArray = vals;
-                    }
-
-
-                    if(!TryRenameHeaders(dataTable)) {
-                        ErrorHandler.HandleError(ErrorType.Alert, "Problem with determining headers.");
-                        return null;
-                    }
-                    UnifyHeaders(dtClone);
-                    return dtClone;
-                }
-
-                File.Delete(FilePaths.tempFolderPath + fileName);
-
-
-                if(!TryRenameHeaders(dataTable)) {
-                    ErrorHandler.HandleError(ErrorType.Alert, "Problem with determining headers.");
-                    return null;
-                }
-                UnifyHeaders(dataTable);
                 return dataTable;
             }
         }
-        
-        private static void UnifyHeaders(DataTable table) {
-            if(table.Columns.Contains("HOUSE") && !table.Columns.Contains(DbHeaders.CUT_HOUSE)) table.Columns["HOUSE"].ColumnName = DbHeaders.CUT_HOUSE;
-            if(table.Columns.Contains("SCHEDULE_DATE_MMDDCCYY") && !table.Columns.Contains(DbHeaders.SCHEDULE_DATE)) table.Columns["SCHEDULE_DATE_MMDDCCYY"].ColumnName = DbHeaders.SCHEDULE_DATE;
-            if(!table.Columns.Contains(DbHeaders.ENTER_DATE)) table.Columns.Add(DbHeaders.ENTER_DATE);
-            if(table.Columns.Contains("ORDER_NO") && !table.Columns.Contains(DbHeaders.ORDER_NUMBER)) table.Columns["ORDER_NO"].ColumnName = DbHeaders.ORDER_NUMBER;
-            if(table.Columns.Contains("ORDER_VOUCH") && !table.Columns.Contains(DbHeaders.VOUCHER)) table.Columns["ORDER_VOUCH"].ColumnName = DbHeaders.VOUCHER;
-            if(table.Columns.Contains("ITEM_NO") && !table.Columns.Contains(DbHeaders.ITEM)) table.Columns["ITEM_NO"].ColumnName = DbHeaders.ITEM;
-            if(table.Columns.Contains("LETTER_SIZE") && !table.Columns.Contains(DbHeaders.SIZE)) table.Columns["LETTER_SIZE"].ColumnName = DbHeaders.SIZE;
-            if(table.Columns.Contains("LETTER_SPEC") && !table.Columns.Contains(DbHeaders.SPEC)) table.Columns["LETTER_SPEC"].ColumnName = DbHeaders.SPEC;
-            if(table.Columns.Contains("NAME") && !table.Columns.Contains(DbHeaders.NAME)) table.Columns["NAME"].ColumnName = DbHeaders.NAME;
-            if(table.Columns.Contains("DRAWING_LETTER_WORD1") && !table.Columns.Contains(DbHeaders.WORD1)) table.Columns["DRAWING_LETTER_WORD1"].ColumnName = DbHeaders.WORD1;
-            if(table.Columns.Contains("DRAWING_LETTER_WORD2") && !table.Columns.Contains(DbHeaders.WORD2)) table.Columns["DRAWING_LETTER_WORD2"].ColumnName = DbHeaders.WORD2;
-            if(table.Columns.Contains("DRAWING_LETTER_WORD3") && !table.Columns.Contains(DbHeaders.WORD3)) table.Columns["DRAWING_LETTER_WORD3"].ColumnName = DbHeaders.WORD3;
-            if(table.Columns.Contains("DRAWING_LETTER_WORD4") && !table.Columns.Contains(DbHeaders.WORD4)) table.Columns["DRAWING_LETTER_WORD4"].ColumnName = DbHeaders.WORD4;
-            if(!table.Columns.Contains(DbHeaders.COLOR1)) table.Columns.Add(DbHeaders.COLOR1);
-            if(!table.Columns.Contains(DbHeaders.COLOR2)) table.Columns.Add(DbHeaders.COLOR2);
-            if(!table.Columns.Contains(DbHeaders.COLOR3)) table.Columns.Add(DbHeaders.COLOR3);
-            if(!table.Columns.Contains(DbHeaders.COLOR4)) table.Columns.Add(DbHeaders.COLOR4);
-            if(!table.Columns.Contains(DbHeaders.RUSH_DATE)) table.Columns.Add(DbHeaders.RUSH_DATE);
 
-            //data.Columns["PARENT_VOUCH"].ColumnName = "";
-            //data.Columns["SCHEDULE_DATE_CCYYMMDD"].ColumnName = "";
+        private static string RenameHeaders(string headerLine) {
+            string[] headers = headerLine.Split(',');
+            for(int i = 0; i != headers.Length; ++i) {
+                headers[i] = TryRenameHeader(headers[i]);
+            }
+            return String.Join(",", headers);
         }
 
-        private static bool TryRenameHeaders(DataTable dataTable) {
-            TryHeaderRename(dataTable, DbHeaders.CUT_HOUSE, new string[] { "house", "hse" });
-            TryHeaderRename(dataTable, DbHeaders.SCHEDULE_DATE, new string[] { "schedule", "sch" });
-            TryHeaderRename(dataTable, DbHeaders.ENTER_DATE, new string[] { "enter", "order*date", "order*dt" });
-            TryHeaderRename(dataTable, DbHeaders.ORDER_NUMBER, new string[] { "order" });
-            TryHeaderRename(dataTable, DbHeaders.VOUCHER, new string[] { "voucher", "vch" });
-            TryHeaderRename(dataTable, DbHeaders.ITEM, new string[] { "item", "style", "code" });
-            TryHeaderRename(dataTable, DbHeaders.SIZE, new string[] { "size", "sze" });
-            TryHeaderRename(dataTable, DbHeaders.SPEC, new string[] { "spec", "width" });
-            TryHeaderRename(dataTable, DbHeaders.NAME, new string[] { "name" });
-            TryHeaderRename(dataTable, DbHeaders.WORD1, new string[] { "word*1" });
-            TryHeaderRename(dataTable, DbHeaders.WORD3, new string[] { "word*2" });
-            TryHeaderRename(dataTable, DbHeaders.WORD4, new string[] { "word*3" });
-            TryHeaderRename(dataTable, DbHeaders.WORD2, new string[] { "word*4" });
-            TryHeaderRename(dataTable, DbHeaders.COLOR1, new string[] { "color*1" });
-            TryHeaderRename(dataTable, DbHeaders.COLOR2, new string[] { "color*2" });
-            TryHeaderRename(dataTable, DbHeaders.COLOR3, new string[] { "color*3" });
-            TryHeaderRename(dataTable, DbHeaders.COLOR4, new string[] { "color*4" });
-            TryHeaderRename(dataTable, DbHeaders.RUSH_DATE, new string[] { "rush", "rsh" });
-            TryHeaderRename(dataTable, DbHeaders.COMMENTS, new string[] { "comment" });
-
-            return true;
-        }
-
-        private static void TryHeaderRename(DataTable dataTable, string dbHeader, string[] tryMatches) {
-            foreach(string lookupString in tryMatches) {
-                //NOTE(adam): convert simple wildcards to regex wildcards and add surrounding matches
-                string pattern = lookupString.ToUpper().Replace("?", ".").Replace("*", "(.+)?");
-                pattern = $"(.+)?{pattern}(.+)?";
-
-                foreach(DataColumn col in dataTable.Columns) {
-                    string colName = col.ColumnName.ToUpper();
-                    if(colName == dbHeader) continue;
-
-                    if(Regex.IsMatch(colName, pattern) && !dataTable.Columns.Contains(dbHeader)) {
-                        col.ColumnName = dbHeader;
-                    }
+        private static string TryRenameHeader(string header) {
+            foreach(FieldData fieldData in FieldData.List) {
+                if(fieldData.IsMatch(header)) {
+                    return fieldData.DbName;
                 }
             }
+            return header;
         }
     }
 }
